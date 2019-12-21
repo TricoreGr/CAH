0728c4d0b73af7f141b45889368cbf8b2f297eee
Giapa <giapa@protonmail.com>
2019-12-21 17:38:35 +0200

    Db up and working.

--- server/gameapp/users/models.py
+++ server/gameapp/users/models.py
@@ -2,14 +2,17 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql:///user.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql:///users' #create a user table in username@localhost
 
 db = SQLAlchemy(app)
 
 class User(db.Model):
     username = db.Column(db.String(120) , primary_key=True, unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    email = db.Column(db.String, unique=True, nullable=False)
-    img = db.Column(db.String, nullable=False, default='default.jpg') #TODO add image
+    email = db.Column(db.String(200), unique=True, nullable=False)
+    img = db.Column(db.String(60), nullable=False, default='default.jpg') #TODO add image
     games = db.Column(db.Integer)
     wins = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.password}', '{self.email}')"
