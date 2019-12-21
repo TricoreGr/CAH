024764eec78c2bf8dc09b8a03818e0d3afcb9464
Giorgos Apostolopoulos <s3apostol@gmail.com>
2019-12-21 19:34:23 +0200

    Set up Vue router

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,12 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+import Play from "../views/Play.vue";
+import Game from "../views/Game.vue";
+import Profile from "../views/Profile.vue";
+import ErrorPage from "../views/ErrorPage.vue";
 
 Vue.use(VueRouter);
 
@@ -11,13 +17,41 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/login",
+    name: "login",
+    component: Login
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: Register
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    component: Profile
+  },
+  {
+    path: "/play",
+    name: "play",
+    component: Play,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: "/play/:gameId",
+    name: "game",
+    component: Game,
+    props: true,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: "*",
+    name: "error",
+    component: ErrorPage
   }
 ];
 
@@ -26,5 +60,6 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 });
+
 
 export default router;
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>HOME</h1>
+    <div id="app">
+      <v-app light>
+        <v-content>
+          <section>
+            <v-parallax :src="img1" height="600">
+              <v-layout column align-center justify-center class="white--text">
+                <img src="assets/vuetify.png" alt="Vuetify.js" height="200" />
+                <h1 class="white--text mb-2 display-1 text-center">
+                  Parallax Template
+                </h1>
+                <div class="subheading mb-4 text-center">
+                  Powered by Vuetify
+                </div>
+                <v-btn
+                  class="mt-12"
+                  color="blue lighten-2"
+                  dark
+                  large
+                  href="/pre-made-themes"
+                >
+                  Get Started
+                </v-btn>
+              </v-layout>
+            </v-parallax>
+          </section>
+
+        </v-content>
+      </v-app>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "home",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      img1: require("@/assets/hero.jpeg")
+    };
   }
 };
 </script>
