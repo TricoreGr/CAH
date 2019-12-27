eb30a3ff620bd2bb303b18507854c98940fad9ee
joj0s <s3apostol@gmail.com>
2019-12-27 19:02:57 +0200

    Clean up code in user services

--- server/gameapp/users/services.py
+++ server/gameapp/users/services.py
@@ -7,10 +7,9 @@ from ..config import Config
 
 def addUser(username,password,email,img):
     exists = User.query.filter_by(username).first()
-
     if exists is None:
-        hashed = hashing(password)
-        user = User(username=username, password=hashed, email=email, img=img)
+        hashedPassword = hashPassword(password)
+        user = User(username=username, password=hashedPassword, email=email, img=img)
 
         db.session.add(user)
         db.session.commit()
@@ -19,7 +18,6 @@ def addUser(username,password,email,img):
             'message' : 'User added to db'
         }
         return jsonify(response)
-
     else:
         response = {
             'message': 'User already exists'
@@ -28,7 +26,6 @@ def addUser(username,password,email,img):
 
 def getUsers():
     users = User.query.all()
-
     if users is None:
         response = {
             'message' : 'No users in database'
@@ -38,12 +35,10 @@ def getUsers():
     results = users_schema.dumps(users)
     results = results.replace('[','').replace(']','')
     data = json.loads(results)
-
     return jsonify(data)
 
 def getUser(uname):
     user = User.query.filter_by(username=uname).first()
-
     if user is None:
         response = {
                 'message' : 'No user in database with this username'
@@ -53,12 +48,11 @@ def getUser(uname):
     results = user_schema.dumps(user)
     results = results.replace('[','').replace(']','')
     data = json.loads(results)
-
     return jsonify(data)
 
 def checkCreds(username,password):
-    hashed = hashing(password)
-    result = User.query.filter_by(username=username).filter_by(password=hashed).first()
+    hashedPassword = hashPassword(password)
+    result = User.query.filter_by(username=username).filter_by(password=hashedPassword).first()
     if result is None:
         response = {
             'message' : 'Invalid credentials'
@@ -70,8 +64,7 @@ def checkCreds(username,password):
         return jsonify({'token': token.decode('utf-8')}) #python encodes it in bytes
         
 
-def hashing(password):
+def hashPassword(password):
     hashpass = hashlib.md5() # create md5 hash
     hashpass.update(password.encode()) #update it with the password
-
     return hashpass.hexdigest() #return the hex
