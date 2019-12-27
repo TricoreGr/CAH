903f35b860dc8018b3eda62cd4c8bed6caf7783a
ApostolosKalovelo <apostoloskalovelonis@gmail.com>
2019-12-27 15:40:59 +0200

    Add comments in Login.vue

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -2,40 +2,46 @@
 import axios from 'axios';
 export default {
   methods: {
-    syncUsername() {
+    syncUsername() { //show username on black card
       if (this.username.length < 20) this.czarName = this.username;
       if (this.username.length == 0) this.czarName = "_________________";
     },
-    syncPassword() {
+    syncPassword() { //show our password gimmick based on the letters of user's typed password
       const text = "veryStr0nkP4ssw0rd";
       var passLength = this.password.length;
       this.czarPassword = text.substring(0, passLength);
       if (passLength == 0) this.czarPassword = "_________________";
     },
-    login(){    
+    login(){
+    //url for post method
     const path = 'http://localhost:5000/users/'+this.username+'/auth';
+    //form rules
     if(this.username && this.username.length <= 20 && this.password.length>0)
+      //use axios for requests
       axios.post(path,{password:this.password})
         .then(res => {
           if(res.data.token){
+            //save the auth token in browser
             localStorage.setItem('authToken',res.data.token);
+            //redirect to homepage
             this.$router.push('/');
           }
           else
+            //auth failed
             this.error = res.data.message;
         })
-        .catch(error =>{
+        .catch(error =>{ //oops
           console.log(error);
         });
       }
   },
   data() {
     return {
-      title: "Login",
-      rules: {
+      rules: { //vuetify form rules
         counter: value => value.length <= 20 || "Max 20 characters",
         required: value => !!value || "Required."
       },
+      //default values for cards
       username : 'IQ^person',
       password : 'lalala',
       czarName : "_________________",
@@ -44,6 +50,7 @@ export default {
     };
   },
   mounted() {
+    //set default text on mount
     this.syncUsername();
     this.syncPassword();
   }
@@ -75,7 +82,7 @@ export default {
               <span class="card__text">
                 and my password
               </span>
-              <span id="czar-password" class="card__input">
+              <span class="card__input">
                 {{czarPassword}}
               </span>
             </div>
@@ -106,6 +113,7 @@ export default {
               </span>
             </div>  
             <div class="card__input-wrapper">
+              <span class="card__input">
               <v-text-field
                 @input="syncPassword"
                 :rules="[rules.required]"
@@ -113,6 +121,7 @@ export default {
                 value="lololo"
                 v-model="password"
               ></v-text-field>
+              </span>
             </div>
           </div>
           <div class = "card__signature-wrapper">
