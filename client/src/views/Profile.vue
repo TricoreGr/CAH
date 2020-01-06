4ab0f698f9c4af8b5579e77f155e0d1b068d7989
ApostolosKalovelo <apostoloskalovelonis@gmail.com>
2020-01-06 17:29:32 +0200

    make profile view responsive and add delete account confirmation overlay

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -11,41 +11,75 @@
         <div class="account__score-wrapper">
           <div class="account__info-wrapper">
             <span class="account__info-label">Wins</span>
-            <span class="account__info">3</span>
+            <span class="account__info">{{wins}}</span>
           </div>
           <div class="account__info-wrapper">
             <span class="account__info-label">Games</span>
-            <span class="account__info">10</span>
+            <span class="account__info">{{gamesTotal}}</span>
           </div>
         </div>
         <div class="account__info-wrapper">
           <h3 class="account__info-label account__info-label--rank">
             Rank
           </h3>
-          <span class="account__info account__info--rank">PASOL</span>
+          <span class="account__info account__info--rank">{{role}}</span>
         </div>
         <div class="account__email-wrapper">
           <span class="account__email-label">email</span>
-          <span class="account__email">sotostosto</span>
+          <span class="account__email">{{email}}</span>
         </div>
       </div>
               <v-btn
             rounded
             dark
+            @click="overlay = true"
             class="account__button account__button--deleteAccount"
           >
             Delete Account?
           </v-btn>
 
     </div>
+    <v-overlay
+          :absolute="absolute"
+          :opacity="opacity"
+          :value="overlay"
+          :z-index="zIndex"
+        >
+        <div class="account__overlay-wrapper">
+        <h3>Are you sure you want to delete your account?</h3>
+        <div class="account__overlay-button-wrapper">
+          <v-btn
+            rounded
+            class="account__cancel-button"
+            @click="overlay = false"
+          >
+            Cancel
+          </v-btn>
+
+          <v-btn
+            color="primary"
+            rounded            
+            @click="overlay = false" 
+          >
+            Confirm
+          </v-btn>
+          </div>
+          </div>
+    </v-overlay>
   </div>
+  
 </template>
 
 <script>
 export default {
   data() {
     return {
-      username: "StoeBlyat"
+      username: "StoeBlyat",
+      email:"gooduser@cah.com",
+      role:"big Boi",
+      wins:3,
+      overlay:false,
+      gamesTotal:10
     };
   }
 };
