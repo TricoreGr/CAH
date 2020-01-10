a7215d44eef6594e3d904964bf2fd0ffbfec0a13
Giapa <giapa@protonmail.com>
2020-01-10 22:52:40 +0200

    Add update to services and routes

--- server/gameapp/users/routes.py
+++ server/gameapp/users/routes.py
@@ -1,46 +1,79 @@
 from flask import Blueprint
-from .services import getUsers, getUser, checkCreds, addUser, getUserByJWToken, deleteUser
+from .services import getUser, checkCreds, addUser, getUserByJWToken, deleteUser, updateUser
 from flask import jsonify,request
 
 users = Blueprint('users', __name__)
 
 @users.route('/delete',methods=['DELETE'])
 def removeUser():
-    token = request.get_json()['token']
-    user = getUserByJWToken(token)
-    username = user['username']
-    response = deleteUser(username)
-    return response
+    try:
+        token = request.get_json()['token']
+        user = getUserByJWToken(token)
+        username = user['username']
+        response = deleteUser(username)
+        return response
+    except:
+        response = {
+            'message' : 'Error at delete'
+        }
 
 @users.route('/signup',methods=['POST'])
 def signup():
-    creds = request.get_json()
-    username = creds['username']
-    password = creds['password']
-    email = creds['email']
-    resp = addUser(username,password,email)
-    return resp
+    try:
+        creds = request.get_json()
+        username = creds['username']
+        password = creds['password']
+        email = creds['email']
+        response = addUser(username,password,email)
+        return response
+    except:
+        response = {
+            'message' : 'Error at singup'
+        }
 
 @users.route('/<username>', methods=['GET'])
 def returnUser(username):
-    user = getUser(username)
-    resp = jsonify(user)
-    return resp
+    try:
+        user = getUser(username)
+        response = jsonify(user)
+        return response
+    except:
+        response = {
+            'message' : 'Error at user'
+        }
 
 @users.route('/jwtToUsername',methods=['POST'])
 def returnUserByJWT():
-    user = getUserByJWToken(request.get_json()['token'])
-    return user
-
-@users.route('/users', methods=['GET'])
-def returnUsers():
-    users = getUsers()
-    return users
+        user = getUserByJWToken(request.get_json()['token'])
+        return user
 
 @users.route('/login', methods=['POST']) #TODO add password 
 def login():
-    creds = request.get_json()
-    username = creds['username']
-    password = creds['password']
-    response = checkCreds(username,password)
-    return response
+    try:
+        creds = request.get_json()
+        username = creds['username']
+        password = creds['password']
+        response = checkCreds(username,password)
+        return response
+    except:
+        response = {
+            'message' : 'Error at login'
+        }
+        return jsonify(response)
+
+@users.route('update',methods=['UPDATE'])
+def update():
+    try:
+        creds = request.get_json()
+        token = creds['token']
+        username = getUserByJWToken(token)
+        username = username['username'] 
+        new_username = creds['username']
+        img = creds['image']
+        response = updateUser(username,new_username,img)
+        return response
+    except:
+        response = {
+            'message':'Error at update'
+        }
+        return jsonify(response)
