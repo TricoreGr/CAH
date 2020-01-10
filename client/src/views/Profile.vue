349c5e8a005538fb029ce007f9b69a8bdf0759fe
Gregory Giapa <43914616+Giapa@users.noreply.github.com>
2020-01-10 23:37:46 +0200

    Update Profile.vue

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -117,7 +117,7 @@ export default {
           this.$router.push("/users/",username);
         })
         .catch(error =>{
-          consol.log(error);
+          console.log(error);
         })
     },
   },
