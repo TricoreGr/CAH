c183b43dccff9add30e55e39576d82dcc9a773e8
Giapa <giapa@protonmail.com>
2020-01-14 14:31:38 +0200

    Add return image in join events

--- server/gameapp/game/events.py
+++ server/gameapp/game/events.py
@@ -1,15 +1,18 @@
 from flask_socketio import SocketIO,emit,send,join_room,leave_room
 from ..socket import socketio
+from ..users.services import returnImg
 
 @socketio.on('joined')
 def joined(data):
     room = data['room']
     join_room(room)
-    emit('playerJoined',{'user':'status',"message":data['username']+' has joined.'},room=room)
+    username = data['username']
+    img = returnImg(username)
+    emit('playerJoined',{'user':'status','message':username +' has joined.', image:img},room=room)
 
 @socketio.on('leave')
 def left(data):
     username = data['username']
     room = data['room']
     leave_room(room)
-    emit('playerLeft',{'user':'status',"message":data['username']+' has left.'}, room=room)
+    emit('playerLeft',{'user':'status','message':data['username']+' has left.'}, room=room)
